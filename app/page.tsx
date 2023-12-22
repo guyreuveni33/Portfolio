e3c9ef0d11d998909ae8f8088220bcb46b327827
guyreuveni33 <guyhaim55@gmail.com>
2023-12-22 11:07:41 +0200

    SectionDivider done with motion, and starting of the About section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/SectionDivider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
       <Intro />
+        <SectionDivider/>
     </main>
   )
 }
