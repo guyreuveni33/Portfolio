367e8d41c3226d365106f908df5647ef073fddd4
guyreuveni33 <guyhaim55@gmail.com>
2023-12-31 21:21:31 +0200

    finish project, all features works. still there is no dark mode option

---
+++ components/projects.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React from "react";
+import SectionHeading from "@/components/sectionHeading";
+import {projectsData} from "@/lib/data";
+import Project from "@/components/project";
+import {useSectionInView} from "@/lib/hooks";
+export default function Projects() {
+    const {ref} = useSectionInView("Projects", 0.5);
+
+    return (
+        <section ref={ref} className="scroll-mt-28" id="projects"><SectionHeading>My projects</SectionHeading>
+            <div>
+                {
+                    projectsData.map((project, index) => (
+                        <React.Fragment key={index}>
+                            <Project {...project}/>
+                        </React.Fragment>
+                    ))
+                }
+            </div>
+        </section>
+    )
+}
+
