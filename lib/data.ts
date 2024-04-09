ace0bbd8c3f916e8e55dd9f5dc80a8becdfa698d
guyreuveni33 <guyhaim55@gmail.com>
2024-04-09 12:38:06 +0300

    I added views for site

--- lib/data.ts
+++ lib/data.ts
@@ -109,22 +109,22 @@ export const projectsData = [
         imageUrl: dic,
         linkUrl:"https://github.com/guyreuveni33/Dynamic-Dictionary",
     },
-    {
+    /*{
         title: "Android Web Chat",
         description:
             "Android Web Chat is a cutting-edge mobile app crafted in Android Studio, offering a smooth and responsive chatting experience.",
         tags: ["Android Development", "Java", "AP 2","MongoDB","Node.js"],
         imageUrl: android,
         linkUrl:"https://github.com/guyreuveni33/WhatsappAP2023/tree/main/MyApplication",
-    },
-    /*{
+    },*/
+    {
         title: "Parallel BFS",
         description:
             "A parallel implementation of the BFS algorithm in C using ThreadsPool for efficient graph traversal.",
         tags: ["C", "ThreadPool", "Parallel Computing","Algorithm"],
         imageUrl: BFS,
         linkUrl:"https://github.com/guyreuveni33/Parallel-BFS",
-    },*/
+    },
 ] as const;
 
 export const skillsData = [
